22c74f03573d9f3f8c60ec4d8c08d82f34131e03
Hikmet05 <hikmet.huseynov1996@mail.ru>
2022-01-13 20:14:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var slideUp = {
+    distance: '100%',
+    origin: 'bottom',
+    opacity: 0
+};
+ScrollReveal().reveal('.animate', {
+    delay: 300,
+    duration: 900,
+    reset: false
+});
+
+ScrollReveal().reveal('.animate', slideUp);
